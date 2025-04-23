0ff92b202b93851d0f7a3131778bb718eb857995
broderickkelly <kelly.bro@northeastern.edu>
2025-04-23 15:53:28 -0400

    Updated test files

--- data_loader_test.py
+++ data_loader_test.py
@@ -2,10 +2,10 @@
 from GRLT_analysis.data_loader import data_loader
 
 y = data_loader(file_path="data/conditions_gastrointestinal.csv")
-y = data_loader(file_path="data/conditions_gastrointestinal.csv")
 y.download_csv()
 y.identify_type()
 y.clean_junk()
 y.select_year("all")
 y.age_death_variable()
+print(y.file)
 
